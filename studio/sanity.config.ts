e5d2cf2cf7482b485ec0d910bbc00769ae046c31
Jeremy Williams <jeremy@vyudu.com>
2025-01-14 00:30:59 -0500

    Update sanity.config.ts

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -10,36 +10,7 @@ export default defineConfig({
   projectId: 'l96yh15e',
   dataset: 'production',
 
-  plugins: [
-    deskTool({
-      defaultDocumentNode: (S) => {
-        // Add preview functionality
-        return S.document().views([
-          S.view.form(),
-          S.view
-            .component(({ document }) => {
-              const { displayed } = document
-              const url = new URL('/api/preview', window.location.origin)
-              url.searchParams.set('type', displayed._type)
-              url.searchParams.set('id', displayed._id)
-              
-              return (
-                <iframe
-                  src={url.toString()}
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    border: 'none',
-                  }}
-                />
-              )
-            })
-            .title('Preview')
-        ])
-      },
-    }),
-    visionTool()
-  ],
+  plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
