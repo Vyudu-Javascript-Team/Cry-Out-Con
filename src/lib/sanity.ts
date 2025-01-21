7be2162c9ddac07cc601a48e49f57b99a78e3dac
Harrison Itotia <harrisonitotia@gmail.com>
2025-01-22 02:43:47 +0300

    add projectId

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  projectId: 'l96yh15e',
   dataset: import.meta.env.VITE_SANITY_DATASET,
   apiVersion: '2021-10-21'
 })
