3d305bb11448c7ef729a434d9f63943dd010c5f4
Harrison Itotia <harrisonitotia@gmail.com>
2025-01-24 03:44:14 +0300

    delete schemas

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,11 +1,8 @@
-import section from './section'
 import speaker from './speaker'
-import page from './page'
-import settings from './settings'
 import hotel from './hotel'
 import conference from './conference'
 import agenda from './agenda'
 import imageSlider from './imageSlider'
 import speakerCategory from './speakerCategory'
 
-export const schemaTypes = [section, speaker, page, settings, conference, agenda, imageSlider, hotel, speakerCategory]
+export const schemaTypes = [speaker, conference, agenda, imageSlider, hotel, speakerCategory]
