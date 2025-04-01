9999668edd4369126c445faad31d108dbcbcef29
Edwin Wanjuki <edwinwanjuki@gmail.com>
2025-04-01 09:09:27 +0300

    Refactor 'Policy.tsx' to use policy page data from backend

--- src/pages/Policy.tsx
+++ src/pages/Policy.tsx
@@ -5,7 +5,6 @@ import { getPolicyPage } from "../lib/sanity";
 type PolicyPageData = {
   heading: string;
   items: [{ title: string; content: string; order: number }];
-  isVisible: boolean;
 };
 
 const Policy = () => {
@@ -28,115 +27,22 @@ const Policy = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 py-16 mt-20">
-      <h1 className="text-4xl font-bold mb-8">Refund Policy & Terms</h1>
+    policyPage && (
+      <div className="container mx-auto px-4 py-16 mt-20">
+        <h1 className="text-4xl font-bold mb-8">{policyPage.heading}</h1>
 
-      <div className="space-y-6">
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Refunds and Exchanges</h2>
-          <p>All ticket sales are final. No refunds or exchanges.</p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Loss Event Badges</h2>
-          <p>
-            Full price will be charged for replacement of lost or misplaced
-            conference badges.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Age Requirement</h2>
-          <p>
-            Attendees must be at least 18 years old to participate in the event.
-            No exceptions.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Duplicate Purchases</h2>
-          <p>
-            To ensure fairness and equal access to all attendees, duplicate
-            ticket purchases are strictly prohibited; each individual is limited
-            to one ticket per registration. Instances of duplicate purchases
-            will result in the cancellation of additional tickets without
-            refund.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Accessibility</h2>
-          <p>
-            We strive to make Cry Out 2025 accessible to all attendees; please
-            contact us in advance to discuss any specific accommodations
-            required.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Health and Safety</h2>
-          <p>
-            The health and safety of all attendees is our top priority, and we
-            will implement measures in accordance with local regulations and
-            guidelines to ensure a safe and comfortable environment for
-            everyone. By attending this event, attendees acknowledge and accept
-            the inherent risks associated with large gatherings and agree to
-            adhere to any health and safety guidelines provided by event
-            organizers; attendees attend at their own risk.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Etiquette and Conduct</h2>
-          <p>
-            All attendees are expected to adhere to our code of conduct,
-            respecting the privacy and boundaries of fellow attendees during the
-            event, and ensuring a respectful and inclusive environment for all
-            participants. Consent should be obtained before exchanging contact
-            information or engaging in post-networking communication.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">
-            Speaker and Performer Substitution
-          </h2>
-          <p>
-            In the event of unforeseen circumstances, organizers reserve the
-            right to substitute scheduled speakers and performers or modify the
-            program without prior notice.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Media Disclosure</h2>
-          <p>
-            Media representatives attending the event must obtain prior approval
-            from event organizers before recording, photographing, or publishing
-            any content related to the event, including sessions, speakers, or
-            attendees.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Intellectual Property</h2>
-          <p>
-            All materials, presentations, songs, and sermons presented at Cry
-            Out 2025 are protected by copyright law and may not be reproduced or
-            distributed without permission from the respective authors.
-          </p>
-        </section>
-
-        <section>
-          <h2 className="text-xl font-semibold mb-2">Social Media</h2>
-          <p>
-            Attendees are permitted to share event-related content on social
-            media platforms, but must respect the privacy and intellectual
-            property rights of other participants, speakers, and performers.
-          </p>
-        </section>
+        <div className="space-y-6">
+          {policyPage.items
+            .sort((item) => item.order)
+            .map((item) => (
+              <section>
+                <h2 className="text-xl font-semibold mb-2">{item.title}</h2>
+                <p>{item.content}</p>
+              </section>
+            ))}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
